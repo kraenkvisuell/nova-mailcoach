8cfabc264c8ba1b7ded78fa1705c631a075f9618
Sebastian Hilger <hilger@kraenk.de>
2020-09-28 09:23:19 +0200

    Update SalutationReplacer.php

--- src/Replacers/SalutationReplacer.php
+++ src/Replacers/SalutationReplacer.php
@@ -16,9 +16,14 @@ class SalutationReplacer implements PersonalizedReplacer
 
     public function replace(string $html, Send $pendingSend): string
     {
-        $salutation = $pendingSend->subscriber->extra_attributes->salutation
-            .' '.$pendingSend->subscriber->extra_attributes->title
-            .' '.$pendingSend->subscriber->last_name;
+        $salutation = 'Sehr geehrte';
+        if ($pendingSend->subscriber->gender == 'm') {
+            $salutation .= 'r Herr ';
+        } else {
+            $salutation .= ' Frau ';
+        }
+        $salutation .= $pendingSend->subscriber->extra_attributes->title
+                       .' '.$pendingSend->subscriber->last_name;
 
         return str_ireplace('::salutation::', $salutation, $html);
     }
